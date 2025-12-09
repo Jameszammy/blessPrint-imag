19d3264844978ade678e99ebd77cf1e88b172256
James S. Zammy <jzammy98@gmail.com>
2025-12-09 15:49:57 +0200

    <chor>:Working on admin dashboard

--- src/js/script.js
+++ src/js/script.js
@@ -89,7 +89,7 @@ item.style.color="";
 
 
 
-const images=['./src/img/shoot.jpeg','./src/img/flex.jpeg'];
+const images=['/src/img/shoot.jpeg','/src/img/flex.jpeg'];
 
 const changeAfterHour = 1;
 
@@ -97,7 +97,7 @@ let lastChange = localStorage.getItem('lastChangeHour');
 let index = parseInt(localStorage.getItem('imageIndex'))||0;
 
 function updateImage(){
-    const today = new Date.now();
+    const today = Date.now();
 
     if(!lastChange||(today - lastChange)>(changeAfterHour * 60 * 60 * 1000)){
         index = (index + 1) % images.length;
